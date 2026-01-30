c757357a4fa218ab27aebb11ab598538b4446878
finder <0xfinder@protonmail.com>
2026-01-31 01:39:38 +0800

    refactor: update footer social links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,26 +4,18 @@ import "./styles/footerStyle.css";
 import { ReactComponent as DiscordSVG } from "../../assets/images/discord.svg";
 import { ReactComponent as GitHubLogoSVG } from "../../assets/images/github.svg";
 import { ReactComponent as TwitterSVG } from "../../assets/images/twitter.svg";
-import { ReactComponent as LinkedInSVG } from "../../assets/images/linkedin.svg";
 
 export function Footer() {
   return (
     <nav className="footer-container">
-      <a target="_blank" rel="noreferrer" href="https://github.com/AndyDanger/live-lol-esports">
+      <a target="_blank" rel="noreferrer" href="https://github.com/0xfinder/live-lol-esports">
         <GitHubLogoSVG className="footer-img" />
       </a>
-      <a target="_blank" rel="noreferrer" href="https://twitter.com/andydangerzone">
+      <a target="_blank" rel="noreferrer" href="https://x.com/0xfinder">
         <TwitterSVG className="footer-img" />
       </a>
-      <a target="_blank" rel="noreferrer" href="https://discord.com/users/183408194209579008">
+      <a target="_blank" rel="noreferrer" href="https://discord.com/users/107390189353103360">
         <DiscordSVG className="footer-img" />
-      </a>
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://www.linkedin.com/in/andrew-marshall-32b404110/"
-      >
-        <LinkedInSVG className="footer-img" />
       </a>
     </nav>
   );
